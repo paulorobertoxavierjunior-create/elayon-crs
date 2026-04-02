4af2d053f9230702656603b833ad4f3f2345382f
paulorobertoxavierjunior-create <paulorobertoxavierjunior@gmail.com>
2026-04-02 02:24:07 -0400

    Criar config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// CONFIGURAÇÃO GLOBAL DO CRS CLOUD
+
+const CRS_CONFIG = {
+  BASE_URL: "COLE_AQUI_URL_DO_SEU_CRS", // EX: https://elayon-crs.onrender.com
+
+  ENDPOINTS: {
+    ANALYZE: "/analyze",
+    CONFIG: "/config",
+    HEALTH: "/health"
+  }
+};
